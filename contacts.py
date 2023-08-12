038683791c23fb95e9597aa684c3628c80471148
ZGalsaqqaf <110371876+ZGalsaqqaf@users.noreply.github.com>
2023-08-12 16:58:45 +0300

    Update contact.py
    Add search function (by name or phone)
    update, delete by id, name, or phone number

--- contacts.py
+++ contacts.py
@@ -7,15 +7,18 @@ class Choice(Enum):
     VIEW = '2'
     DELETE = '3'
     UPDATE = '4'
-    EXIT = '5'
+    SEARCH = '5'
+    EXIT = '6'
 
 Choice_values = [member.value for member in Choice]
 
 # -------------------------------------------------------------
+
 print('-' * 35, "\n\tYOUR CONTACT LIST")
 print('-' * 35, "\n") 
 
 # -------------------------------------------------------------
+
 file_path = 'contacts.json'
 user_choice =""
 # -------------------------------------------------------------
@@ -43,6 +46,18 @@ def check_repeate(name, phone):
             if phone in value.values():
                 return f"\nThis phone number ( {phone} ) is repeated! \nAgain please: \n"
             
+    return False
+# ------------------------------------------------------------------------------------------
+
+def get_id_by_name_phone(input1):
+    with open(file_path, 'r') as getdata:
+        data = json.load(getdata)
+
+        for key, value in data.items():
+            if input1 in value.values():
+                id = key
+                return id,value['name'],value['phone']  # returns tuple
+    
     return False
    
 # ------------------------------------------------------------------------------------------
@@ -80,71 +95,90 @@ def add_data():
 
             with open(file_path, 'w') as save:
                 json.dump(data, save)   # python to json
-                print("Successfully Added.\n")
+                print("\nSuccessfully Added.\n")
 
 # ---------------------------------------------------
 
 def view_data():
     with open(file_path, 'r') as view:
         data = json.load(view)
-        for key, value in data.items():
-            for k, v in value.items():
-                print(f"{k.title()}: ", end=" ")
-                print(f"{v.title()}")
-            print('\n')
+        if data:
+            for key, value in data.items():
+                for k, v in value.items():
+                    print(f"{k.title()}: ", end=" ")
+                    print(f"{v.title()}")
+                print('\n')
+        else:
+            print("\nNo Contacts to show!\n")
 
 # ---------------------------------------------------
 
 def delete_data():
-    id = input("Enter Contact id: ")
+    user_input = input("Enter an id, name, or phone number: ").strip()
+    contact_info = get_id_by_name_phone(user_input)
 
-    with open(file_path, 'r') as getdata:
-        data = json.load(getdata)
-
-        if id in data:
+    if contact_info:
+        with open(file_path, 'r') as getdata:
+            data = json.load(getdata)
+            id = contact_info[0]
             deleted_cont = data.pop(id) 
 
             with open(file_path, 'w') as delete:
                 data_1 = json.dump(data, delete)
                 print(f"{deleted_cont['name']} was deleted.\n")
-        else:
-            print("This Id is not exists!\n")
+    else:
+        print(f"This contact ( {user_input} ) is not exists!\n")
 
 # ---------------------------------------------------
 
 def update_data():
     view_data()
 
-    id = input("Enter Contact id: ")
+    user_input = input("Enter an id, name, or phone number: ").strip()
+    contact_info = get_id_by_name_phone(user_input)
 
-    with open(file_path, 'r') as getdata:
-        data = json.load(getdata)
+    if contact_info:
+        with open(file_path, 'r') as getdata:
+            data = json.load(getdata)
 
-        if id in data:
             name = input("Enter Now Name: ").strip().lower()
             phone = input("Enter Now Phone: ").strip()
 
-            if check_name_phone(name, phone):
+            if name == contact_info[1] and phone == contact_info[2]:
+                print("\nNo change happened. \n")
+            
+            elif name != contact_info[1] and phone != contact_info[2]:
+                if check_repeate(name, phone):
+                    print(check_repeate(name, phone))
+                    switch()
+
+            elif (check_name_phone(name, phone)):
                 print(check_name_phone(name, phone))
                 switch()
 
-            elif check_repeate(name, phone):
-                print(check_repeate(name, phone))
-                switch()
             
             else:
                 dic = {
-                    "id" : id, 
+                    "id" : contact_info[0], 
                     "name" : name,
                     "phone" : phone,
                 }
-                data[id] = dic
+                data[contact_info[0]] = dic
                 with open(file_path, 'w') as update:
-                    json.dump(data, update)
-                    print(f"Successfully Updated.\n")
-
+                    data1 = json.dump(data, update)
+                    print(f"\nSuccessfully Updated.\n")
+    else:
+        print(f"This contact ( {user_input} ) is not exists!\n")
+        switch()
 # -------------------------------------------------------------------
 
+def search_data():
+    user_input = input("Enter an id, name, or phone number to search for: ").strip()
+    contact_info = get_id_by_name_phone(user_input)
+    if contact_info:
+        print(f"Name: {contact_info[1]} \nPhone: {contact_info[2]}")
+    
+# -------------------------------------------------------------------
 def switch():
     if user_choice == Choice.ADD.value:
         add_data()
@@ -154,6 +188,8 @@ def switch():
         delete_data()
     elif user_choice == Choice.UPDATE.value:
         update_data()
+    elif user_choice == Choice.SEARCH.value:
+        search_data()
     else:
         print("Thank You ^^ \n\n")
         exit()
@@ -163,7 +199,14 @@ def switch():
 # ------------------------------------------------------------------------------------
 
 while user_choice != Choice.EXIT.value:
-    print("1. Add \n2. View \n3. Delete \n4. Update \n5. Exit\n")
+
+    s = '''
+------------------------------------------------------------------------
+---------------------**   Main Menue    **------------------------------
+------------------------------------------------------------------------
+'''
+    print(s)
+    print("1. Add \n2. View \n3. Delete \n4. Update \n5. Search \n6. Exit")
 
     user_choice = input("\nEnter Choice: ").strip()
     print("\n")
